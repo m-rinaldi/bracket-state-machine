8c60564c96aa1faeef61ca8a68331a7bcf4bfac4
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-08-28 17:56:25 +0200

    initial State, Transition and StateMachine implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,120 @@
+#![allow(unused)]
+
+use bracket_lib::prelude::*;
+
+#[derive(Copy, Clone)]
+pub struct Ms(f32);
+
+#[derive(Copy, Clone)]
+pub struct DeltaTime(Ms);
+
+pub enum Transition {
+    Switch(Box<dyn State>),
+    Push(Box<dyn State>),
+    Pop,
+    Reset,
+    // TODO Sequence(Vec<Transition>)
+}
+
+
+pub trait State {
+    #[must_use]
+    fn handle_input(&mut self, input: VirtualKeyCode) -> Option<Transition>;
+
+    #[must_use]
+    fn update(&mut self, dt: DeltaTime) -> Option<Transition>;
+
+    fn draw(&self, term: &mut BTerm);
+}
+
+pub struct StateMachine {
+    states: Vec<Box<dyn State>>,
+    pending_ops: Vec<Transition>,
+}
+
+impl StateMachine {
+    // TODO implement From<State>
+    /// creates a state machine with an initial state
+    pub fn new<T: State + 'static>(init_state: T) -> Self {
+        StateMachine {
+            states: vec![Box::new(init_state)],
+            pending_ops: vec![],
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.states.is_empty()
+    }
+
+    pub fn handle_input(&mut self, input: VirtualKeyCode) {
+        if let Some(state) = self.states.last_mut() {
+            if let Some(trans) = state.handle_input(input) {
+                self.pending_ops.push(trans);
+            }
+        }
+    }
+
+    // TODO fn switch()
+
+    pub fn update(&mut self, dt: DeltaTime) {
+        if let Some(top_state) = self.states.last_mut() {
+            let trans = top_state.update(dt);
+            if let Some(trans) = trans {
+                self.pending_ops.push(trans);
+            }
+        }
+    }
+
+    pub fn draw(&self, mut screen: &mut BTerm) {
+        for state in self.states.iter() {
+            state.draw(screen);
+        }
+    }
+
+    // TODO write these individual operations
+
+    fn apply_pending_trans(&mut self) {
+        let pending_ops = self.pending_ops.drain(..);
+        for pending_op in pending_ops {
+            match pending_op {
+                Transition::Switch(new_state) => {
+                    self.states.pop();
+                    self.states.push(new_state);
+                }
+                Transition::Push(new_state) => {
+                    self.states.push(new_state)
+                }
+                Transition::Pop => {
+                    self.states.pop();
+                }
+                Transition::Reset => {
+                    self.states.clear();
+                }
+            };
+        }
+    }
+}
+
+impl GameState for StateMachine {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        if (self.is_empty()) {
+            ctx.quit();
+        }
+
+        if let Some(key) = ctx.key {
+            self.handle_input(key);
+        }
+
+        // TODO time
+        self.update(DeltaTime(Ms(33.0)));
+
+        ctx.cls();
+        self.draw(ctx);
+
+        // TODO perform pending operations
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
